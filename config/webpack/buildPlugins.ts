87e0dc69838f33b3903b8e0ad80e915d254e1171
Maxim <zeetreex@gmail.com>
2024-01-19 22:07:50 +0200

    fork ts checker plugin

--- config/webpack/buildPlugins.ts
+++ config/webpack/buildPlugins.ts
@@ -2,12 +2,14 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
 import { ProgressPlugin, Configuration } from "webpack";
 import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
 import { WebpackConfig } from "./types";
+import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
 
 export const buildPlugins = ({ paths, isDev }: WebpackConfig): Configuration["plugins"] => {
 	const plugins: Configuration["plugins"] = [
 		new HtmlWebpackPlugin({
 			template: paths.html,
 		}),
+		new ForkTsCheckerWebpackPlugin(),
 	];
 
 	if (isDev) {
