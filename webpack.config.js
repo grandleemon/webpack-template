2aaeac5e37ac126a4485f38eaba9c8927a3b534b
Maxim <zeetreex@gmail.com>
2024-01-19 20:11:43 +0200

    typescript, ts-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,31 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
 
 module.exports = {
 	mode: "production",
-	entry: path.resolve(__dirname, "src", "index.js"),
+	entry: path.resolve(__dirname, "src", "index.ts"),
 	output: {
 		path: path.resolve(__dirname, "build"),
 		filename: "bundle.[contenthash].js",
-		clean: true
+		clean: true,
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
-			template: path.resolve(__dirname, "public", "index.html")
-		})
-	]
+			template: path.resolve(__dirname, "public", "index.html"),
+		}),
+		new webpack.ProgressPlugin(),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: "ts-loader",
+				exclude: /node_modules/,
+			},
+		],
+	},
+	resolve: {
+		extensions: [".tsx", ".ts", ".js"],
+	},
 };
